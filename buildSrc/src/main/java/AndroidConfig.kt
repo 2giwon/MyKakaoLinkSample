3fcfafdd28991b7550fc19508d4581201909a8e7
Giwon Lee <2giwon@gmail.com>
2023-07-10 21:53:23 +0900

    [Add] buildSrc

---
+++ buildSrc/src/main/java/AndroidConfig.kt
@@ -0,0 +1,7 @@
+object AndroidConfig {
+    const val VERSION = "1.0.0"
+    const val TARGET_SDK = 33
+    const val MIN_SDK = 24
+    const val APP_ID = "com.egiwon.mykakaolinktest"
+    const val VERSION_CODE = 100
+}
